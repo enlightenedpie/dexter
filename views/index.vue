4b010127d34dc1aeea8c6511b62579132717e9ed
gabrieljsch <gschneid@mit.edu>
2017-07-17 14:42:35 -0700

    Operation Pretty

--- views/index.vue
+++ views/index.vue
@@ -1,29 +1,29 @@
 <template lang="html">
-    <div>
+    <div id="parentdiv">
         <div id="toolbar">
             <ul>
                 <li> <button id='modal-button' v-on:click="showModal = true"> Menu </button> </li>
-                <li><img src="https://supertutortv.com/wp-content/uploads/2016/10/sttv_site_logo.png" height=55 class="logo"/></li>
+                <li><img src="https://supertutortv.com/wp-content/uploads/2016/10/sttv_site_logo.png" height=40 class="logo"/></li>
                 <li> Hi, {{user.profile.firstName}} </li>
             </ul>
         </div>
-        <div id="NavBar">
-        <h3>Buffer so I can see what the hell I'm working on</h3>
-        <h3>{{currentCourse}}</h3>
-            <a>
-                <ul v-for="(item, index) in user.courses">
-                <li v-for="(subitem, subindex) in item">
-                    <a v-if="index === currentCourse">{{subindex}}</a>
-                </li>
-                </ul>
-            </a>
+        <div class="maindiv">
+            <div id="NavBar">
+            <h3>{{currentCourse}}</h3>
+                <a>
+                    <ul v-for="(item, index) in user.courses">
+                    <li v-for="(subitem, subindex) in item">
+                        <a v-if="index === currentCourse">{{subindex}}</a>
+                    </li>
+                    </ul>
+                </a>
+            </div>
+            <div>
+
+            </div>
+
+            <modal v-if="showModal" v-on:close="showModal = false" :user="user" :currentcourse="currentCourse" :changecourse="switchCourse"> </modal>
         </div>
-        <div>
-
-        </div>
-
-        <modal v-if="showModal" v-on:close="showModal = false" :user="user" :currentcourse="currentCourse" :changecourse="switchCourse"> </modal>
-
     </div>
 </template>
 
