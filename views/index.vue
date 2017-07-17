c30fc687fe30a20b429661db9c027c4beb47e8ce
nathanwenger <nedwenger@gmail.com>
2017-07-17 13:48:09 -0700

    Added a course nav bar

--- views/index.vue
+++ views/index.vue
@@ -2,11 +2,25 @@
     <div>
         <div id="toolbar">
             <ul>
-                <li> <h1>{{currentCourse}}</h1> </li>
                 <li> <button id='modal-button' v-on:click="showModal = true"> Menu </button> </li>
+                <li><img src="https://supertutortv.com/wp-content/uploads/2016/10/sttv_site_logo.png" height=55 class="logo"/></li>
                 <li> Hi, {{user.profile.firstName}} </li>
             </ul>
             <modal v-if="showModal" v-on:close="showModal = false" :user="user" :currentcourse="currentCourse" :changecourse="switchCourse"> </modal>
+        </div>
+        <div id="NavBar">
+        <h3>Buffer so I can see what the hell I'm working on</h3>
+        <h3>{{currentCourse}}</h3>
+            <a>
+                <ul v-for="(item, index) in user.courses">
+                <li v-for="(subitem, subindex) in item">
+                    <a v-if="index === currentCourse">{{subindex}}</a>
+                </li>
+                </ul>
+            </a>
+        </div>
+        <div>
+
         </div>
 
     </div>
@@ -25,21 +39,5 @@ export default {
             this.currentCourse = course;
             },
     }
-
-
-
-
 };
-//     data: function() {
-//         return {
-//         }
-//     }
-// }
-// let curCourse = new Vue ({
-//     el: ".maindiv",
-//     data: {
-//         currentCourse: "ACT"
-//     }
-// });
-
 </script>
