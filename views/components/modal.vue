12d221d36e8b240c4018ae09e265c36c84d1b73c
gabrieljsch <gschneid@mit.edu>
2017-07-18 09:12:17 -0700

    Navbar looks like a navbar now

--- views/components/modal.vue
+++ views/components/modal.vue
@@ -1,85 +1,84 @@
 <template lang="html">
-<div>
-<div>        <div id="toolbar">
-            <ul>
-                <li> <button v-on:click="modalswitch(true)" id='modal-button'> Menu </button> </li>
-                <li><img src="https://supertutortv.com/wp-content/uploads/2016/10/sttv_site_logo.png" height=40 class="logo"/></li>
-                <li> Hi, {{ user.profile.firstName }} </li>
-            </ul>
-        </div>
-</div>
-  <div v-if="showmodal" id="Orders" class="tabcontent">
-    <div class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-          <div class="modal-body">
-            <div class="tab">
-              <button v-on:click="makeactive('profile')">Profile</button>
-              <button v-on:click="makeactive('orders')">Orders</button>
-              <button v-on:click="makeactive('courses')">Courses</button>
-              <button v-on:click="makeactive('settings')">Settings</button>
-            </div>
-            <ul>
-
-              <div v-if="active === 'profile'">
-                <h3>Profile</h3>
-                <img :src="user.profile.image" height="80">
-                <p>{{user.profile.firstName}} {{user.profile.lastName}}</p>
+  <div>
+    <div id="toolbar">
+      <ul>
+          <li> <button v-on:click="modalswitch(true)" id='modal-button'> Menu </button> </li>
+          <li><img src="https://supertutortv.com/wp-content/uploads/2016/10/sttv_site_logo.png" height=40 class="logo"/></li>
+          <li> Hi, {{ user.profile.firstName }} </li>
+      </ul>
+    </div>
+    <div v-if="showmodal" id="Orders" class="tabcontent">
+      <div class="modal-mask">
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-body">
+              <div class="tab">
+                <button v-on:click="makeactive('profile')">Profile</button>
+                <button v-on:click="makeactive('orders')">Orders</button>
+                <button v-on:click="makeactive('courses')">Courses</button>
+                <button v-on:click="makeactive('settings')">Settings</button>
               </div>
+              <ul>
 
-              <div v-if="active === 'orders'">
-                <h3>Orders</h3>
-                <ul>
-                  <li v-for="(item, index) in user.orders">
-                    {{index}}
-                    <ul>
-                      <li>- {{item}}</li>
-                    </ul>
+                <div v-if="active === 'profile'">
+                  <h3>Profile</h3>
+                  <img :src="user.profile.image" height="80">
+                  <p>{{user.profile.firstName}} {{user.profile.lastName}}</p>
+                </div>
+
+                <div v-if="active === 'orders'">
+                  <h3>Orders</h3>
+                  <ul>
+                    <li v-for="(item, index) in user.orders">
+                      {{index}}
+                      <ul>
+                        <li>- {{item}}</li>
+                      </ul>
+                      </li>
+                  </ul>
+                </div>
+
+                <div v-if="active === 'courses'">
+                  <h3>Courses</h3>
+                  <ul>
+                    <li v-for="(item, index) in user.courses">
+                    <button class="course-buttons" v-on:click="switchcourse(index)"> {{index}} </button>
                     </li>
-                </ul>
-              </div>
-
-              <div v-if="active === 'courses'">
-                <h3>Courses</h3>
-                <ul>
-                  <li v-for="(item, index) in user.courses">
-                  <button class="course-buttons" v-on:click="switchcourse(index)"> {{index}} </button>
-                  </li>
-                </ul>
-              </div>
-
-              <div v-if="active === 'settings'">
-                <h3>Settings</h3>
-                <a>Recommend Content</a>
-                <div class="onoffswitch">
-                    <input type="checkbox" name="suggestcontent" id="recommendContent"
-                         v-model="user.settings.recommendContent">
+                  </ul>
                 </div>
-                <br>
-                <br>
-                <a>Reset password</a>
-                <div class="passwordreset">
-                  <input type="text" name="resetpassword">
-                    <br>
-                    <input type="text" name="resetcheck">
+
+                <div v-if="active === 'settings'">
+                  <h3>Settings</h3>
+                  <a>Recommend Content</a>
+                  <div class="onoffswitch">
+                      <input type="checkbox" name="suggestcontent" id="recommendContent"
+                           v-model="user.settings.recommendContent">
+                  </div>
+                  <br>
+                  <br>
+                  <a>Reset password</a>
+                  <div class="passwordreset">
+                    <input type="text" name="resetpassword">
+                      <br>
+                      <input type="text" name="resetcheck">
+                  </div>
+                  <br>
+                  <br>
+                  <a>Offline Content</a>
+                  <a><div class="onoffswitch">
+                    <input type="checkbox" name="suggestcontent" class="onoffswitch-checkbox" id="offlineContent"
+                              v-model="user.settings.offlineContent">
+                    <span class="onoffswitch-inner"></span>
+                    <span class="onoffswitch-switch"></span>
+                  </div></a>
                 </div>
-                <br>
-                <br>
-                <a>Offline Content</a>
-                <a><div class="onoffswitch">
-                  <input type="checkbox" name="suggestcontent" class="onoffswitch-checkbox" id="offlineContent"
-                            v-model="user.settings.offlineContent">
-                  <span class="onoffswitch-inner"></span>
-                  <span class="onoffswitch-switch"></span>
-                </div></a>
-              </div>
-            </ul>
+              </ul>
+            </div>
+            <button class="modal-default-button" v-on:click="modalswitch(false)"> Confirm </button>
           </div>
-          <button class="modal-default-button" v-on:click="modalswitch(false)"> Confirm </button>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
--- views/components/navbar.vue
+++ views/components/navbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <div id="NavBar" class="navbar">
-        <h3>{{currentcourse}}</h3>
-        <a>
-        <div id="navbar-items">
-            <ul v-for="(item, index) in user.courses">
-                <li v-for="(subitem, subindex) in item.subjects">
-                    <a v-if="index === currentcourse">{{subitem}}</a>
-                </li>
-            </ul>
+    <div>
+        <div id="NavBar" class="navbar">
+            <h3>{{currentcourse}}</h3>
         </div>
-        </a>
+        <div id="navbar-items">
+                <ul v-for="(item, index) in user.courses">
+                    <li v-for="(subitem, subindex) in item.subjects">
+                        <a v-show="index === currentcourse">{{subitem}}</a>
+                    </li>
+                </ul>
+        </div>
     </div>
 </template>
 
