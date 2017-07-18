1e556dfff6fee64a50ee497d38b6e559b4ad5c06
gabrieljsch <gschneid@mit.edu>
2017-07-18 12:00:12 -0700

    CSS hotfix navbar

--- views/components/modal.vue
+++ views/components/modal.vue
@@ -2,9 +2,9 @@
   <div>
     <div id="toolbar">
       <ul>
-          <li> <button class='modal-button'> Menu </button> </li>
+          <li> <button class='modal-button'> <img id="menu-icon" src="https://image.flaticon.com/icons/png/512/56/56763.png"> Menu </button> </li>
           <li><img src="https://supertutortv.com/wp-content/uploads/2016/10/sttv_site_logo.png" height=40 class="logo"/></li>
-          <li> <button class='modal-button' v-on:click="modalswitch(true)"> Hi, {{ user.profile.firstName }} </button> </li>
+          <li> <button class='modal-button' v-on:click="modalswitch(true)"> <img src="https://cdn0.iconfinder.com/data/icons/account-avatar/128/user_2-512.png" height="14"> {{ user.profile.firstName }} </button> </li>
       </ul>
     </div>
     <div v-if="showmodal" id="Orders" class="tabcontent">
@@ -13,20 +13,20 @@
           <div class="modal-container">
             <div class="modal-body">
               <div class="tab">
-                <button v-on:click="makeactive('profile')">Profile</button>
-                <button v-on:click="makeactive('orders')">Orders</button>
-                <button v-on:click="makeactive('courses')">Courses</button>
-                <button v-on:click="makeactive('settings')">Settings</button>
+                <button v-on:click="makeActiveCourse('profile')">Profile</button>
+                <button v-on:click="makeActiveCourse('orders')">Orders</button>
+                <button v-on:click="makeActiveCourse('courses')">Courses</button>
+                <button v-on:click="makeActiveCourse('settings')">Settings</button>
               </div>
               <ul>
 
-                <div v-if="active === 'profile'">
+                <div v-if="activecourse === 'profile'">
                   <h3>Profile</h3>
                   <img :src="user.profile.image" height="80">
                   <p>{{user.profile.firstName}} {{user.profile.lastName}}</p>
                 </div>
 
-                <div v-if="active === 'orders'">
+                <div v-if="activecourse === 'orders'">
                   <h3>Orders</h3>
                   <ul>
                     <li v-for="(item, index) in user.orders">
@@ -38,7 +38,7 @@
                   </ul>
                 </div>
 
-                <div v-if="active === 'courses'">
+                <div v-if="activecourse === 'courses'">
                   <h3>Courses</h3>
                   <ul>
                     <li v-for="(item, index) in user.courses">
@@ -47,7 +47,7 @@
                   </ul>
                 </div>
 
-                <div v-if="active === 'settings'">
+                <div v-if="activecourse === 'settings'">
                   <h3>Settings</h3>
                   <a>Recommend Content</a>
                   <div class="onoffswitch">
@@ -80,20 +80,18 @@
       </div>
     </div>
         <div id="video-holder">
-            <iframe width="100%" height="500" v-bind:src="vidsrc" frameborder="0" allowfullscreen></iframe>
+            <iframe width="100%" height="650" v-bind:src="vidsrc" frameborder="0" allowfullscreen></iframe>
         </div>
-        <div>
         <div id="NavBar" class="navbar">
             <h3>{{currentcourse}}</h3>
-        </div>
         <div id="navbar-items">
-                <ul v-for="(item, index) in user.courses">
-                    <li v-if="index === currentcourse" v-bind:class="{active: index === currentcourse}" v-for="(subitem, subindex) in item.subjects">
-                        <a>{{subitem}}</a>
-                    </li>
-                </ul>
+          <ul class="nav-items">
+            <li class="active" v-for="(item, index) in user.courses[currentcourse].subjects" v-on:click="makeActiveTab(item)">
+              <a> {{item}} </a>
+            </li>
+          </ul>
         </div>
-    </div>
+        </div>
   </div>
 </template>
 
@@ -101,13 +99,17 @@
 export default {
     data() {
       return {
-        active: 'profile'
+        activecourse: 'profile',
+        activetab: ''
             }
     },
     methods: {
-      makeactive: function(item){
-                this.active = item;
-            }
+      makeActiveCourse: function(item){
+                this.activecourse = item;
+            },
+        makeActiveTab: function(item){
+            this.activetab = item;
+        }
     },
     props: ['currentcourse','user','switchcourse', 'showmodal', 'modalswitch', 'vidsrc'],
     components: ['modal']
--- views/components/videos.vue
+++ views/components/videos.vue
@@ -4,9 +4,13 @@
         <ul>
             <li v-for="(item, index) in vidlist">
                 <a v-bind:href="item.uri">
-                    <img class="thumbnails" v-bind:src="item.thumbnail">
-                    {{item.name}}</a>
+                    <div>
+                        <p> {{item.name}} </p>
+                        <img class="thumbnails" v-bind:src="item.thumbnail">
+                    </div>
+                </a>
             </li>
+        </ul>
     </div>
 </template>
 
