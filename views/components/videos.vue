bc4f965a87248631845333824b09ee9e87ed4e35
nathanwenger <nedwenger@gmail.com>
2017-08-01 11:35:15 -0700

    Fixed conflict

--- views/components/videos.vue
+++ views/components/videos.vue
@@ -5,25 +5,6 @@
             <iframe width="75%" height="540" :src="preplink(vidsrc.link)" frameborder="0" allowfullscreen></iframe>
         </div>
         <div class="sidebar">
-<<<<<<< HEAD
-            <ul>
-              <li v-for="(index, item) in subtablist" v-on:click="make_active(item)"
-                  v-bind:class="{active : isactive === item}">
-                <a v-show="item != 'type'">{{format(item)}}</a>
-              </li>
-            </ul>
-        </div>
-        <div class="linkeronis">
-            <ul>
-                <li v-for="(item, index) in vidlist" v-bind:class="{active : item.link === vidsrc.link}"
-                    v-on:click="switch_vid(item); make_active_video(index)">
-                    <div class="subvideo-holder">
-                        <p> {{format(item.name)}}</p>
-                        <img class="thumbnails" :src="item.pictures.link">
-                        <div class="time-holder">
-                            <p class="times"> {{temporize(item.duration)}} </p>
-                        </div>
-=======
           <li v-for="(index, item) in subtablist" v-on:click="make_active(item)" v-bind:class="{active : isactive === item}">
             <a v-show="item != 'type'">{{format(item)}}</a>
           </li>
@@ -35,7 +16,6 @@
                     <img class="thumbnails" :src="item.pictures.link">
                     <div class="time-holder">
                         <p class="times"> {{temporize(item.duration)}} </p>
->>>>>>> 31a1ee675d25e1b61fc1219a7b7be47f3cd39ad8
                     </div>
                 </div>
             </li>
